261a5e51f3e7abdb981daba5c226fa1b3d5b276e
CB <cnalangan@gmail.com>
2018-04-04 08:06:32 +0800

    Initial commit.

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+let app = express();
+
+app.use(express.static(publicPath));
+// console.log(__dirname + '/../public');
+// console.log(publicPath);
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
+});
