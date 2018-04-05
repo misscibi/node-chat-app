16baf673b0308ca6cba8fccb497ae9ee57965801
CB <cnalangan@gmail.com>
2018-04-05 23:17:10 +0800

    Scroll to bottom if user is close to bottom.

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,22 @@
 // initiating a request from client to server to open a web socket and keep that connection open
 const socket = io();
 
+const scrollToBottom = () => {
+    // Selectors
+    const messages = jQuery('#messages');
+    const newMessage = messages.children('li:last-child');
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 // ES6 features ain't working for other browsers
 socket.on('connect', () => {
     console.log('Connected to server.');
@@ -21,6 +37,7 @@ socket.on('newMessage', (message) => {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', (message) => {
